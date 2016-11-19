300a2ccd56c93f079bc73aa9e9c500d817a738d2
anderswag <anderson.hung@live.com>
2016-11-19 17:35:25 -0500

    RouteAuthenticate

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,4 +1,18 @@
 class SignupController < ApplicationController
   def index
   end
+
+  def authenticate
+    #render json: { test: 'test' }
+    Plaid.config do |p|
+      p.client_id = '583085cc46eb125da3dd7838'
+      p.secret = 'bec7d76950f9edda762d9aa1eb88f8'
+      p.env = :tartan  # or :production
+    end
+    render json: {thing: user = Plaid::User.create(:connect, 'wells', 'plaid_test', 'plaid_good')}
+
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'signup/index'
-
+  get 'signup/authenticate', to: 'signup#authenticate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
